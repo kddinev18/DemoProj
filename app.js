c2431e32cf3d71ce03cc543bf97e8b5d282fd751
kddinev18 <kddinev18@codingburgas.bg>
2022-11-24 10:16:58 +0200

    Add js functionality

---
+++ app.js
@@ -0,0 +1,11 @@
+const accordion = document.querySelectorAll('.content-container');
+
+console.log(accordion)
+
+for(i=0;i<accordion.length;i++)
+{
+    accordion[i].addEventListener('click', function()
+    {
+        this.classList.toggle('active');
+    });
+}
